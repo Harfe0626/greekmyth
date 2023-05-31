c648ceda50026beec43b49397befd67fe53f66ab
Harfe0626 <strik241@gmail.com>
2023-05-31 17:28:15 +0900

    2023/05/31

--- practices/5534165_신정엽_연습문제02/practice7.php
+++
@@ -1,7 +0,0 @@
-<?php 
-/*Q. 변수 $name을 출력하는 <?php echo $name?>의 약식 표현은 무엇인가?*/
-
-//A. -1번- 
-echo "Q7. 변수 $ name을 출력하는 <?php echo $ name?>의 약식 표현은 무엇인가?<br>";
-echo "1번"; /*<?=$name?>*/
-?>
--- practices/5534165_신정엽_연습문제04/practice4-5.php
+++
@@ -1,9 +0,0 @@
-<?php 
-    $fact = 1;
-    for($num=1; $num<=10; $num++)
-    {
-        $fact *= $num;
-
-        echo "{$num}! = $fact<br>";
-    }
-?>
